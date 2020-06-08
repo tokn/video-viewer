ad8930f0d70559ad229739cc2544b0ce70ee0b33
tokn <tokn@users.noreply.github.com>
2020-06-08 08:31:53 +0100

    Initial scaffolding

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Video from '../views/Video.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/video/:id',
+    name: 'Video',
+    component: Video
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
